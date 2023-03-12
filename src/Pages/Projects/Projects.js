9c8dc19a8f98f0bd7e2810ea905a4841635ecdf8
cse-mominul <cse.mominul.islam>
2023-03-12 21:08:43 +0600

    first commit

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -26,7 +26,11 @@ const Projects = () => {
                             <div className="badge text-white badge-outline">Firebase</div>
                         </div>
                     </div>
-                    <Link to=''><button className=" w-full border-2  shadow-white text-white py-3 rounded w-36 font-bold">View Details</button></Link>
+                    <div>
+                    <a href='https://lenesqueen-a0269.web.app/'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Live Site</button></a>
+                    <a href='https://github.com/cse-mominul/LensQueen'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Client Site</button></a>
+                    <a href='https://github.com/cse-mominul/LensQueenServerSite'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Server Site</button></a>
+                    </div>
                 </div>
 
                 <div data-aos="flip-right" className="bgColor2 card  shadow-md bg-blend-hard-light  border-b-2  shadow-white border-white rounded-md p-2  ">
@@ -46,7 +50,12 @@ const Projects = () => {
                             <div className="badge text-white badge-outline">Firebase</div>
                         </div>
                     </div>
-                    <Link to=''><button className=" w-full border-2  shadow-white text-white py-3 rounded w-36 font-bold">View Details</button></Link>
+                    <div>
+                    <a href='https://zeron-a55e8.web.app/'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Live Site</button></a>
+                    <a href='https://github.com/cse-mominul/zeron'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Client Site</button></a>
+                    <a href='https://github.com/cse-mominul/ZeronServer'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Server Site</button></a>
+                    </div>
+                    
                 </div>
 
                 <div data-aos="flip-right" className="bgColor2 card  shadow-md bg-blend-hard-light  border-b-2  shadow-white border-white rounded-md p-2  ">
@@ -66,7 +75,11 @@ const Projects = () => {
                             <div className="badge text-white badge-outline">Firebase</div>
                         </div>
                     </div>
-                    <Link to=''><button className=" w-full border-2  shadow-white text-white py-3 rounded w-36 font-bold">View Details</button></Link>
+                    <div>
+                    <a href='https://more-and-more-3661c.web.app/'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Live Site</button></a>
+                    <a href='https://github.com/cse-mominul/reseler'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Client Site</button></a>
+                    <a href='https://github.com/cse-mominul/ReselarServer'><button className="ml-5 border-2  shadow-white text-white py-3 rounded p-2 font-bold">Server Site</button></a>
+                    </div>
                 </div>
             </div>
         </div>
