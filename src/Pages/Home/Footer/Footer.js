2ea1781457dec25c12027c9ff161b60d94845a3c
cse-mominul <cse.mominul.islam>
2023-03-13 11:49:41 +0600

    first commit

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <footer className="footer footer-center font-bold text-bold p-4 bg-white  text-blue-800">
              <MessengerCustomerChat
-                pageId="<103460092385786>"
-                appId="<724354469165711>"
+                pageId="103460092385786"
+                appId="724354469165711"
             />,
             <div>
                 <p>Copyright © 2023 - All right reserved by Mominul Islam</p>
